63b511d95f357a7fd0b0b22d72311cba3cf2d58b
PurviMurkute <purvimurkute51@gmail.com>
2025-06-06 18:34:19 +0530

    file modified

--- server/controllers/tvshow.js
+++ server/controllers/tvshow.js
@@ -1,37 +1,77 @@
 import TvShow from "../models/TvShow.js";
 
 const getTvShow = async (req, res) => {
+  const tvShow = await TvShow.find();
 
-    const tvShow = await TvShow.find();
-
-    res.status(201).json({
-        success: true,
-        data: tvShow,
-        message: "All shows fetched successfully"
-    })
-}
+  res.status(200).json({
+    success: true,
+    data: tvShow,
+    message: "All shows fetched successfully",
+  });
+};
 
 const postTvShow = async (req, res) => {
-    const {title, channel, timing, thumbnail} = req.body;
+  const { title, channel, timing, thumbnail } = req.body;
 
-    const newShow = new TvShow({
-        title,
-        channel,
-        timing,
-        thumbnail
+  const newShow = new TvShow({
+    title,
+    channel,
+    timing,
+    thumbnail,
+  });
+  const savedTvShow = await newShow.save();
+
+  res.status(201).json({
+    success: true,
+    data: savedTvShow,
+    message: "New show added successfully",
+  });
+};
+
+const getTvShowsById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const tvShowbyId = await TvShow.findOne({ _id: id }); //findById(id)
+
+    if(!tvShowbyId){
+        return res.status(404).json({
+            success: false,
+            data: null,
+            message: "Tv Show not found"
+        })
     }
-)
-    const savedTvShow = await newShow.save();
 
-    res.status(201).json({
-        success: true,
-        data: savedTvShow,
-        message: "New show added successfully"
-    })
+    return res.status(200).json({
+      success: true,
+      data: tvShowbyId,
+      message: "Tv show fetched successfully",
+    });
+  } catch (e) {
+    return res.status(400).json({
+      success: false,
+      data: null,
+      message: e.message,
+    });
+  }
+};
 
-}
+const deleteTvShowbyId = async (req, res) => {
+  const { id } = req.params;
 
-export {
-    getTvShow,
-    postTvShow
-}
+  try {
+    await TvShow.deleteOne({ _id: id }); //findByIdAndDelete(id)
+
+    return res.status(200).json({
+      success: true,
+      message: "Tv show deleted successfully",
+    });
+  } catch (e) {
+    res.status(400).json({
+      success: false,
+      message: e.message,
+    });
+  }
+};
+
+export { getTvShow, postTvShow, getTvShowsById, deleteTvShowbyId };
--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 
 dotenv.config(); //To use environment variables in your Node.js (Express) app, you import and configure the dotenv package.
 
-import { getTvShow, postTvShow } from './controllers/tvshow.js';
+import { getTvShow, postTvShow, getTvShowsById, deleteTvShowbyId } from './controllers/tvshow.js';
 
 const app = express();
 app.use(express.json());
@@ -31,8 +31,9 @@ app.get('/health', (req, res)=>{
 })
 
 app.get('/tvshows', getTvShow)
-
 app.post('/addshows', postTvShow)
+app.get('/tvshow/:id', getTvShowsById)
+app.delete('/tvshow/:id', deleteTvShowbyId)
 
 app.listen(PORT, () => {
     console.log(`server is running on PORT ${PORT}`)
