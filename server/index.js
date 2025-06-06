fca4cc5fab56a2ad12601d07b169c036646a74de
PurviMurkute <purvimurkute51@gmail.com>
2025-06-06 11:10:46 +0530

    mongodb connected

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); //To use environment variables in your Node.js (Express) app, you import and configure the dotenv package.
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = 5002;
+const PORT = process.env.PORT || 5002;
+
+const connectDB = async () => {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+
+    if(conn){
+        console.log("MongoDB Connected")
+    }else{
+        console.log("MongoDB not Connected")
+    }
+}
+
+const TV_SHOWS = [];
 
 app.get('/health', (req, res)=>{
     res.status(201).json({
@@ -14,10 +30,35 @@ app.get('/health', (req, res)=>{
     })
 })
 
-app.post('/addmedia', (req, res) => {
-    const {name, } = req.body
+app.get('/tvshows', (req, res) => {
+    res.status(201).json({
+        success: true,
+        data: TV_SHOWS,
+        message: "All shows fetched successfully"
+    })
+})
+
+app.post('/addshows', (req, res) => {
+    const {title, channel, timing, thumbnail} = req.body;
+
+    const newShow = {
+        title,
+        channel,
+        timing,
+        thumbnail
+    }
+
+    TV_SHOWS.push(newShow);
+
+    res.status(201).json({
+        success: true,
+        data: newShow,
+        message: "New show added successfully"
+    })
+
 })
 
 app.listen(PORT, () => {
     console.log(`server is running on PORT ${PORT}`)
+    connectDB();
 })
